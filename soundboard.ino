1bfb21d050108674a792b4b18a661697710972ea
tsrtjh <itanada02@gmail.com>
2026-02-13 09:48:22 +0200

    Update soundboard.ino

--- soundboard.ino
+++ soundboard.ino
@@ -57,7 +57,22 @@
      * This helper macro is defined in `i2s_common.h` and shared by all the I2S communication modes.
      * It can help to specify the I2S role and port ID */
     i2s_chan_config_t chan_cfg;
-    i2s_std_config_t std_cfg;
+    i2s_std_config_t std_cfg = {
+        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(44100), //48000
+        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
+        .gpio_cfg = {
+            .mclk = I2S_GPIO_UNUSED,
+            .bclk = I2S_BCLK,
+            .ws = I2S_LRC,
+            .dout = I2S_DOUT,
+            .din = I2S_GPIO_UNUSED,
+            .invert_flags = {
+                .mclk_inv = false,
+                .bclk_inv = false,
+                .ws_inv = false, //WS 0 -> Left Channel
+            },
+        },
+    };
 
 //------------------------------------------------------------------------------------------------------------------------
 
