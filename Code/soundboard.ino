47dddf001fbfd15e0b813e52635e18ab9bcb5d6f
tsrtjh <itanada02@gmail.com>
2026-02-14 00:42:07 +0200

    Update soundboard.ino

--- Code/soundboard.ino
+++ Code/soundboard.ino
@@ -33,35 +33,42 @@
     const int NOTPRESSED = -1;                    // Arbitrary value denoting that no key was detected (must be less than 0 or greater than 3)
     
     const uint8_t COLS_NUM = 2;                   // Amount of column pins = log2(Actual amount of used columns in keypad), rounded up
-    const uint8_t ROWS_NUM = 3;                   // Amount of row pins 
+    const uint8_t ROWS_NUM = 3;                   // Amount of row pins = log2(Actual amount of used columns in keypad + 1), rounded up
+    const uint8_t WAV_ARRAY_SIZE = 16;            // The size of the Wav array, should be equal to the number of buttons in the keypad 
     const int COLS[COLS_NUM] = {8, 9};            // Define the column pins, green -> left, blue -> right
     const int ROWS[ROWS_NUM] = {21, 20, 10};      // Define the row pins, most significant to the left 
     static const i2s_port_t i2s_num = I2S_NUM_0;  // i2s port number
 
+    // Keyboard wav file names array
+    const String WAV_ARRAY[WAV_ARRAY_SIZE] = {"/dong_48000.wav", "/dong_48000.wav", "/Lame_Boom.wav", "",
+                                  "", "", "", "",
+                                  "", "", "", "",
+                                  "", "", "", ""};
+
     // Debugging with serial
-    const bool KB_DEBUG = false;
-    const bool I2S_DEBUG = true;
+    const bool KB_DEBUG = false;                  // Debugging Keyboard presses
+    const bool I2S_DEBUG = true;                  // Debugging I2S file reading
 //------------------------------------------------------------------------------------------------------------------------
 
 // Global variables
     // Keypad
-    int currentColumn = 0;                        // Storing the current "column cycle"
-    bool PRESSED = false;                         // Boolean if a button is pressed
-    int pressedTimer = 0;                         // Cooldown of button presses
+    int CurrentColumn = 0;                        // Storing the current "column cycle"
+    bool Pressed = false;                         // Boolean if a button is pressed
+    int PressedTimer = 0;                         // Cooldown of button presses
+    int PressedButton = NOTPRESSED;
 
     // Wav files
-    int fileSize=0;                               // Current played file size 
+    int FileSize=0;                               // Current played file size 
     File WavFile;                                 // Object for root of SD card directory
-    bool FILE_LOADED = false;
+    bool FileLoaded = false;
 
     // I2S configuration
-
-    i2s_chan_handle_t tx_handle;
+    i2s_chan_handle_t TX_Handle;
     /* Get the default channel configuration by the helper macro.
      * This helper macro is defined in `i2s_common.h` and shared by all the I2S communication modes.
      * It can help to specify the I2S role and port ID */
-    i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
-    i2s_std_config_t std_cfg = {
+    i2s_chan_config_t Chan_Cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
+    i2s_std_config_t Std_Cfg = {
         .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(44100), //48000
         .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
         .gpio_cfg = {
@@ -85,18 +92,24 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Starting!");
+  Serial.println("Start initializing!");
   SDCardInit();
   keyboardInit();
   I2SInit();
+  for(int i = 0; i<WAV_ARRAY_SIZE; i++){
+    Serial.print(i);
+    Serial.print(" :\"");
+    Serial.print(WAV_ARRAY[i]);
+    Serial.println("\"");
+  }
 }
 
 void loop() {
-    if(!PRESSED){
+    if(!Pressed){
       keyboardRoutine();
     }else{
-      if(!FILE_LOADED){
-        FILE_LOADED = loadWavFile("/dong_48000.wav");
+      if(!FileLoaded){
+        FileLoaded = loadWavFile(WAV_ARRAY[PressedButton]);
       }else{
         PlayWav();
       }
@@ -115,11 +128,22 @@ void keyboardInit(){
   }
 }
 
+void SDCardInit(){   
+    //digitalWrite(SD_CS, HIGH); // SD card chips select, must use GPIO 5 (ESP32 SS)
+    Serial.println("SD card initializing");
+    if(!SD.begin(SD_CS)){
+        Serial.println("Error talking to SD card!");
+        while(true);                  // end program
+    }else{
+      Serial.println("Done talking");
+    }
+}
+
 void I2SInit(){
   Serial.println("initializing i2s TX channel");
-  i2s_new_channel(&chan_cfg, &tx_handle, NULL);
-  i2s_channel_init_std_mode(tx_handle, &std_cfg);
-  i2s_channel_enable(tx_handle);
+  i2s_new_channel(&Chan_Cfg, &TX_Handle, NULL);
+  i2s_channel_init_std_mode(TX_Handle, &Std_Cfg);
+  i2s_channel_enable(TX_Handle);
 }
 
 void powerCols(int currentColumn){
@@ -162,12 +186,13 @@ int readRows(){
 }
 
 void keyboardRoutine(){
-    
+  
   //Power columns
-  powerCols(currentColumn);
+  powerCols(CurrentColumn);
 
   //Read from rows
-  if(!PRESSED){
+  if(!Pressed){
+    PressedButton = NOTPRESSED;
     int currentRow = readRows();
     if(KB_DEBUG){
       Serial.print("Current Row: ");
@@ -180,19 +205,26 @@ void keyboardRoutine(){
       Serial.print("Found match on row #");
       Serial.print(currentRow);
       Serial.print(" and column #");
-      Serial.println(currentColumn);
+      Serial.println(CurrentColumn);
       Serial.print("Value of key is: ");
-      Serial.println(currentRow*4 + currentColumn + 1);
-      PRESSED = true;
+      PressedButton = currentRow*4 + CurrentColumn;
+      Serial.println(PressedButton + 1);
+
+      // If the program got an out of bounds button pressed value
+      if(PressedButton < 0 or PressedButton > WAV_ARRAY_SIZE){
+        Serial.println("Button pressed is invalid");
+        PressedButton = NOTPRESSED;
+      }
+      Pressed = true;
       delay(200);
     }
   }else{
-    pressedTimer = pressedTimer + 1;
+    PressedTimer = PressedTimer + 1;
     
     //if timer is over cooloff limit
-    if(pressedTimer > COOLOFF){
-      pressedTimer=0;
-      PRESSED = false;
+    if(PressedTimer > COOLOFF){
+      PressedTimer=0;
+      Pressed = false;
     }
   }
     
@@ -200,26 +232,25 @@ void keyboardRoutine(){
   delay(TICKLENGTH);
     
   // Checking next column
-  currentColumn = (currentColumn + 1) % 4;
+  CurrentColumn = (CurrentColumn + 1) % 4;
 }
 
 bool loadWavFile(String fileName){
   
   // get the wav file from the SD card
-  WavFile = SD.open(fileName);                          // Open the wav file
+  WavFile = SD.open(fileName);                    // Open the wav file
   if(WavFile==false){
     Serial.print("Could not open ");
     Serial.print(fileName);
+    Pressed = false;                              // Set the button state to "not pressed" to get a new press value.
   }else{
-    WavFile.read((byte *) &WavHeader,44);               // Read in the WAV header, which is first 44 bytes of the file. 
-                                                        // We have to typecast to bytes for the "read" function
-    DumpWAVHeader(&WavHeader);                          // Dump the header data to serial, optional!
-    if(ValidWavData(&WavHeader))                        // optional if your sure the WAV file will be valid.
+    WavFile.read((byte *) &WavHeader,44);         // Read in the WAV header, which is first 44 bytes of the file. 
+                                                  // We have to typecast to bytes for the "read" function
+    DumpWAVHeader(&WavHeader);                    // Dump the header data to serial, optional!
+    if(ValidWavData(&WavHeader))                  // optional if your sure the WAV file will be valid.
       Serial.print("Wav header good, sample rate: ");
       Serial.println(WavHeader.SampleRate);
-      fileSize=WavHeader.DataSize;
-      //i2s_set_sample_rates(i2s_num, WavHeader.SampleRate);      //set sample rate
-      //std_cfg.clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(WavHeader.SampleRate);
+      FileSize=WavHeader.DataSize;
   }
   Serial.println(WavFile == false);
   return !(WavFile == false);
@@ -227,84 +258,77 @@ bool loadWavFile(String fileName){
 
 void PlayWav()
 {
-  static bool ReadingFile=true;                       // True if reading file from SD. false if filling I2S buffer
-  static byte Samples[NUM_BYTES_TO_READ_FROM_FILE];   // Memory allocated to store the data read in from the wav file
-  static uint16_t BytesRead;                          // Num bytes actually read from the wav file which will either be
-                                                      // NUM_BYTES_TO_READ_FROM_FILE or less than this if we are very
-                                                      // near the end of the file. i.e. we can't read beyond the file.
-  //Serial.println(Samples[0]);
+  static bool ReadingFile=true;                   // True if reading file from SD. false if filling I2S buffer
+  static byte Samples[NUM_BYTES_TO_READ_FROM_FILE]; // Memory allocated to store the data read in from the wav file
+  static uint16_t BytesRead;                      // Num bytes actually read from the wav file which will either be
+                                                  // NUM_BYTES_TO_READ_FROM_FILE or less than this if we are very
+                                                  // near the end of the file. i.e. we can't read beyond the file.
 
-  if(ReadingFile)                                     // Read next chunk of data in from file if needed
+  if(ReadingFile)                                 // Read next chunk of data in from file if needed
   {
-  // Serial.println("Reading");
-    BytesRead=ReadFile(Samples);                      // Read data into our memory buffer, return num bytes read in
-    ReadingFile=false;                                // Switch to sending the buffer to the I2S
+    BytesRead=ReadFile(Samples);                  // Read data into our memory buffer, return num bytes read in
+    ReadingFile=false;                            // Switch to sending the buffer to the I2S
   }
   else
-    ReadingFile=FillI2SBuffer(Samples,BytesRead);     // We keep calling this routine until it returns true, at which point
-  //  Serial.println("Filling");                        // this will swap us back to Reading the next block of data from the file.
-                                                      // Reading true means it has managed to push all the data to the I2S 
-                                                      // Handler, false means there still more to do and you should call this
-                                                      // routine again and again until it returns true.
+    ReadingFile=FillI2SBuffer(Samples,BytesRead); // We keep calling this routine until it returns true, at which point
+                                                  // this will swap us back to Reading the next block of data from the file.
+                                                  // Reading true means it has managed to push all the data to the I2S 
+                                                  // Handler, false means there still more to do and you should call this
+                                                  // routine again and again until it returns true.
 }
 
 uint16_t ReadFile(byte* Samples){
-    static uint32_t BytesReadSoFar=0;                   // Number of bytes read from file so far
-    uint16_t BytesToRead;                               // Number of bytes to read from the file
+    static uint32_t BytesReadSoFar=0;             // Number of bytes read from file so far
+    uint16_t BytesToRead;                         // Number of bytes to read from the file
     
     if(BytesReadSoFar+NUM_BYTES_TO_READ_FROM_FILE>WavHeader.DataSize)   // If next read will go past the end then adjust the 
       BytesToRead=WavHeader.DataSize-BytesReadSoFar;                    // amount to read to whatever is remaining to read
     else
-      BytesToRead=NUM_BYTES_TO_READ_FROM_FILE;                          // Default to max to read
+      BytesToRead=NUM_BYTES_TO_READ_FROM_FILE;    // Default to max to read
       
-    WavFile.read(Samples,BytesToRead);                  // Read in the bytes from the file
-    BytesReadSoFar+=BytesToRead;                        // Update the total bytes red in so far
+    WavFile.read(Samples,BytesToRead);            // Read in the bytes from the file
+    BytesReadSoFar+=BytesToRead;                  // Update the total bytes red in so far
     
-    if(BytesReadSoFar>=WavHeader.DataSize)              // Have we read in all the data?
-    {
-      WavFile.seek(44);                                 // Reset to start of wav data  
-      BytesReadSoFar=0;                                 // Clear to no bytes read in so far                            
+    if(BytesReadSoFar>=WavHeader.DataSize){       // Have we read in all the data?
+      WavFile.seek(44);                           // Reset to start of wav data  
+      BytesReadSoFar=0;                           // Clear to no bytes read in so far                            
     }
-    if(I2S_DEBUG){
+    if(I2S_DEBUG){                                // Debugging info
       Serial.print("File Size: ");
-      Serial.println(fileSize);
+      Serial.println(FileSize);
       Serial.print("Bytes read so far: ");
       Serial.println(BytesReadSoFar);
       Serial.print("Bytes %: ");
-      Serial.print(BytesReadSoFar*100/fileSize);
+      Serial.print(BytesReadSoFar*100/FileSize);
      Serial.println("%");
     }
     
     // File done reading
-    if(BytesReadSoFar >= fileSize - NUM_BYTES_TO_READ_FROM_FILE){
+    if(BytesReadSoFar >= FileSize - NUM_BYTES_TO_READ_FROM_FILE){
       Serial.println("Done reading file!");
-      PRESSED = false;
-      FILE_LOADED = false;
+      Pressed = false;
+      FileLoaded = false;
     }
-    return BytesToRead;                                 // return the number of bytes read into buffer
+    return BytesToRead;                           // return the number of bytes read into buffer
 }
 
 bool FillI2SBuffer(byte* Samples,uint16_t BytesInBuffer){
-    // Writes bytes to buffer, returns true if all bytes sent else false, keeps track itself of how many left
-    // to write, so just keep calling this routine until returns true to know they've all been written, then
-    // you can re-fill the buffer
+    /** Writes bytes to buffer, returns true if all bytes sent else false, keeps track itself of how many left
+        to write, so just keep calling this routine until returns true to know they've all been written, then
+        you can re-fill the buffer **/
     
-    size_t BytesWritten;                        // Returned by the I2S write routine, 
-    static uint16_t BufferIdx=0;                // Current pos of buffer to output next
-    uint8_t* DataPtr;                           // Point to next data to send to I2S
-    uint16_t BytesToSend;                       // Number of bytes to send to I2S
+    size_t BytesWritten;                          // Returned by the I2S write routine, 
+    static uint16_t BufferIdx=0;                  // Current pos of buffer to output next
+    uint8_t* DataPtr;                             // Point to next data to send to I2S
+    uint16_t BytesToSend;                         // Number of bytes to send to I2S
     
-    // To make the code eaier to understand I'm using to variables to some calculations, normally I'd write this calcs
-    // directly into the line of code where they belong, but this make it easier to understand what's happening
+    /** To make the code eaier to understand I'm using to variables to some calculations, normally I'd write this calcs
+        directly into the line of code where they belong, but this make it easier to understand what's happening **/
     
-    DataPtr=Samples+BufferIdx;                               // Set address to next byte in buffer to send out
-    BytesToSend=BytesInBuffer-BufferIdx;                     // This is amount to send (total less what we've already sent)
-    //Serial.print("Bytes to send: ");
-    //Serial.println(BytesToSend);
-    i2s_channel_write(tx_handle,DataPtr,BytesToSend,&BytesWritten,1);  // Send the bytes, wait 1 RTOS tick to complete
-    BufferIdx+=BytesWritten;                                 // increasue by number of bytes actually written
-    //Serial.print("Bytes in buffer: ");
-    //Serial.println(BytesInBuffer);
+    DataPtr=Samples+BufferIdx;                    // Set address to next byte in buffer to send out
+    BytesToSend=BytesInBuffer-BufferIdx;          // This is amount to send (total less what we've already sent)
+    i2s_channel_write(TX_Handle,DataPtr,BytesToSend,&BytesWritten,1);  // Send the bytes, wait 1 RTOS tick to complete
+    BufferIdx+=BytesWritten;                      // increasue by number of bytes actually written
     if(BufferIdx>=BytesInBuffer)                 
     {
       // sent out all bytes in buffer, reset and return true to indicate this
@@ -312,21 +336,9 @@ bool FillI2SBuffer(byte* Samples,uint16_t BytesInBuffer){
       return true;                             
     }
     else
-      return false;       // Still more data to send to I2S so return false to indicate this
+      return false;                               // Still more data to send to I2S so return false to indicate this
 }
 
-void SDCardInit(){   
-    //Serial.println(SD_CS);
-    //pinMode(SD_CS, OUTPUT); 
-    //digitalWrite(SD_CS, HIGH); // SD card chips select, must use GPIO 5 (ESP32 SS)
-    Serial.println("SD card initializing");
-    if(!SD.begin(SD_CS)){
-        Serial.println("Error talking to SD card!");
-        while(true);                  // end program
-    }else{
-      Serial.println("Done talking");
-    }
-}
 
 bool ValidWavData(WavHeader_Struct* Wav){
   if(memcmp(Wav->RIFFSectionID,"RIFF",4)!=0) 
